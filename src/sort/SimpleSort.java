ca4e2e2fef322eda23080d37e7614038f06c7f91
Gabriel Ulrich <ulrichgw06@mounties.mansfield.edu>
2017-04-19 07:38:49 -0500

    Big data initial commit

---
+++ src/sort/SimpleSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+/**
+ * Big-O time of O(n*n)
+ *
+ * @author 55jphillip
+ */
+public class SimpleSort {
+
+    public static void bubbleSort(int[] a, String direction) {
+        int out, in;
+        boolean moreChanges = true;
+
+        for (out = a.length - 1; out > 0 && moreChanges; out--) {
+            moreChanges = false;
+            for (in = 0; in < out; in++) {
+                if (a[in] > a[in + 1] && direction.equalsIgnoreCase("A")
+                        || a[in] < a[in + 1] && direction.equalsIgnoreCase("D")) {
+                    int temp = a[in];
+                    a[in] = a[in + 1];
+                    a[in + 1] = temp;
+                    moreChanges = true;
+                }
+            }
+        }
+    }
+
+}
